164861c27e81d987a86b2f8e4f69caa61521155a
Ming <rf37535@gmail.com>
2017-06-06 19:33:45 +0800

    Speedup maxpooling by factor 10

--- layers/maxpooling.py
+++ layers/maxpooling.py
@@ -6,31 +6,24 @@ class Maxpooling:
         self.kernel_width = kw
         self.kernel_height = kh
         self.mask = []
-    
-    def _op_maxpooling(self, in_map):
-        output_shape = np.array(in_map.shape) / np.array([self.kernel_width, self.kernel_height])
-        output = np.zeros(output_shape)
-        mask = np.zeros(in_map.shape)
-        for i in xrange(output_shape[0]):
-            for j in xrange(output_shape[1]):
-                interest = in_map[i * self.kernel_width  : i * 2 + self.kernel_width
-                                ,j * self.kernel_height : j * 2 + self.kernel_height]
-                output[i][j] = interest.max()
-                idx = interest.argmax()
-                mask[i * self.kernel_width + idx / self.kernel_width ] \
-                    [j * self.kernel_height + idx % self.kernel_height] = 1
-        self.mask.append(mask)
-        return output
 
     def _forward(self, x):
         # Cache the input for backward use
         self.input = copy.deepcopy(x)
-        output = []
-        for i in x:
-            imm_result = []
-            for j in i:
-                imm_result.append(self._op_maxpooling(j))
-            output.append(imm_result)
-        return np.array(output)
+        
+        B, N, h, w = x.shape
+        
+        tmp = x.reshape(B, N, h/self.kernel_width, self.kernel_width, w/self.kernel_height, self.kernel_height).swapaxes(3, 4).reshape(B, N, h/self.kernel_width, w/self.kernel_height, self.kernel_width * self.kernel_height)
+        
+        self.masks = np.zeros(x.shape)
+        indexes = tmp.argmax(axis=-1)
+        for b in xrange(indexes.shape[0]):
+            for i in xrange(indexes.shape[1]):
+                for x in xrange(indexes.shape[2]):
+                    for y in xrange(indexes.shape[3]):
+                        index = indexes[b][i][x][y]
+                        self.masks[b][i][x * self.kernel_width + index / self.kernel_width ][y * self.kernel_height + index % self.kernel_height] = 1
+
+        return tmp.max(axis=-1)
             
 
