e559aece8c4941020323d664e9b769b8ebdfbf3e
Ming <rf37535@gmail.com>
2017-06-06 16:27:39 +0800

    Implement fully-connected layer

--- layers/linear.py
+++ layers/linear.py
@@ -1,2 +1,19 @@
+import numpy as np
+
 class Linear:
-    pass
+    def __init__(self, i, c):
+        self.input_size = i
+        self.classes = c
+        
+        # Random kernel initialization
+        self.weights = np.random.rand(i, c)
+        self.bias = np.random.rand()
+    
+    def _forward(self, x):
+        # Cache the input for backward use
+        self.input = x
+        output = []
+        for i in x:
+            imm_result = np.dot(np.transpose(self.weights), i) + self.bias
+            output.append(imm_result)
+        return np.array(output)
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from net import Net
 import numpy as np
 
 if __name__ == '__main__':
-    x = np.random.rand(500, 1, 28, 28)
+    x = np.random.rand(5000, 1, 28, 28)
     y = [0]*50
     net = Net()
     net.push(Conv2d(3, 3, 1, 3)) # 1x28x28 -> 3x26x26
     net.push(Relu())
     net.push(Reshape((2028, 1)))
-    net.push(Softmax(2028, 10))
+    net.push(Linear(2028, 512))
+    net.push(Relu())
+    net.push(Linear(512, 64))
+    net.push(Relu())
+    net.push(Softmax(64, 10))
     net.input(x, y, 'train')
     net.forward()
     #net.debug() 
