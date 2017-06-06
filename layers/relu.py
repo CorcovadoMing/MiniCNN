ef05a2f310b0f8b47751acaf579f11b04fae8ba6
Ming <rf37535@gmail.com>
2017-06-06 16:17:10 +0800

    Implement relu activator

--- layers/relu.py
+++ layers/relu.py
@@ -1,2 +1,10 @@
 class Relu:
-    pass
+    def __init__(self):
+        pass
+    
+    def _forward(self, x):
+        # Be careful the shape changes here, so [0] used to unwrap
+        x = x * [x > 0][0]
+        # Fix the -0.0 issue
+        x += 0.
+        return x
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     y = [0]*50
     net = Net()
     net.push(Conv2d(3, 3, 1, 3)) # 1x28x28 -> 3x26x26
+    net.push(Relu())
     net.push(Reshape((2028, 1)))
     net.push(Softmax(2028, 10))
     net.input(x, y, 'train')
