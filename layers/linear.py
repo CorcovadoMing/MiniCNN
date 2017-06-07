Implement backward of reshape layer; fix minor formula

layers/linear.py
@@ -21,7 +21,7 @@ class Linear:
 
     def _backward(self, err, res):
         self.d_weights = np.dot(np.multiply(err, res).T, self.input).T
-        self.d_bias = err.sum()
+        self.d_bias = np.multiply(err, res).sum()
         return np.dot(err, self.weights.T), None
     
     def _update(self, step):

layers/reshape.py
@@ -9,5 +9,5 @@ class Reshape:
         self.output_shape = [x.shape[0]] + self.shape
         return np.reshape(x, self.output_shape)
     
-    def _backward(self, e):
-        pass
+    def _backward(self, err, res):
+        return err.reshape(self.input_shape), None