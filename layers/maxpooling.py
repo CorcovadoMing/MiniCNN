55d539bc64bdfb70d6ff00616cf212ff17238212
Ming <rf37535@gmail.com>
2017-06-10 13:30:04 +0800

    Update maxpooling layer

--- layers/maxpooling.py
+++ layers/maxpooling.py
@@ -1,6 +1,6 @@
 import copy
 import numpy as np
-from poolop import pool_bc01, bprop_pool_bc01
+from poolop import maxpooling, unpooling
 
 class Maxpooling:
     def __init__(self, kw, kh):
@@ -11,14 +11,14 @@ class Maxpooling:
         B, N, h, w = x.shape
         output = np.empty((B, N, h/self.kernel_width, w/self.kernel_height))
         switch = np.empty(output.shape + (2,), dtype=np.int)
-        pool_bc01(x, output, switch, self.kernel_width, self.kernel_height, 2, 2)
+        maxpooling(x, output, switch, self.kernel_width, self.kernel_height, 2, 2)
         self.masks = switch
         self.input_shape = x.shape
         return output
     
     def _backward(self, err, res):
         output = np.empty(self.input_shape)
-        bprop_pool_bc01(err, self.masks, output)
+        unpooling(err, self.masks, output)
         return output, None
     
     def _update(self, *args):
