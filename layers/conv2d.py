fb17e5df040ffbb3825831e979f8404afdae7706
Ming <rf37535@gmail.com>
2017-06-10 18:43:56 +0800

    Implement bias on conv2d layer

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -15,7 +15,7 @@ class Conv2d:
         self.pd_weight = 0
         # Random kernel initialization
         self.weights = np.random.normal(0, 0.1, (input_channel, output_channel, kw, kh))
-        self.bias = np.random.rand()
+        self.bias = np.random.normal(0, 0.01, (1, output_channel, 1, 1))
 
     def _forward(self, x):
         # Cache the input for backward use
@@ -37,7 +37,7 @@ class Conv2d:
         out_map_size = list(x.shape[:1]) + list(self.weights.shape[1:2]) + list(out_map_size)
         output = np.empty(out_map_size)
         conv2d_op(x, self.weights, output)
-        return output
+        return output + self.bias
         
                 
     def _rot180(self, kernel):
@@ -60,6 +60,7 @@ class Conv2d:
         self.d_weights = np.zeros_like(self.weights)
         output = np.zeros_like(self.input)
         deconv2d_op(self._rot180(self.input), err, self._rot180(self.weights), output, self.d_weights)
+        self.d_bias = (np.sum(err, axis=(0, 2, 3)) / err.shape[0])[None, :, None, None]
         return output, None
         
     
@@ -69,3 +70,4 @@ class Conv2d:
         self.weights += var
         self.weights -= (decay * self.weights)
         #self.weights -= step * self.d_weights
+        self.bias -= step * self.d_bias
