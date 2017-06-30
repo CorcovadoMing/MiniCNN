b203edb3f90782bb921fb0a5a21ca8f6065ab6fb
Ming <rf37535@gmail.com>
2017-06-30 21:56:03 +0800

    Fix minor issue on leaky relu

--- layers/leaky_relu.py
+++ layers/leaky_relu.py
@@ -3,15 +3,13 @@ import numpy as np
 class LeakyRelu:
     def __init__(self):
         pass
-    
+
     def _forward(self, x):
-        pos = (x > 0).astype(float)
-        neg = (x < 0).astype(float) * 0.01
-        self.mask = (pos + neg)
+        self.mask = (x >= 0).astype(float) + (x < 0).astype(float) * 0.1
         return x * self.mask
-    
+
     def _backward(self, err, res):
         return err, self.mask
-    
+
     def _update(self, *args):
         pass
