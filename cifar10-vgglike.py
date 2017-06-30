a116ff0c48d5f134325a15bbb3d1dba1e73f39fd
Ming <rf37535@gmail.com>
2017-06-30 21:57:52 +0800

    Modify cifar10 example

--- cifar10-vgglike.py
+++ cifar10-vgglike.py
@@ -1,4 +1,4 @@
-from layers import Conv2d, Linear, Maxpooling, Relu, LeakyRelu, Softmax, Reshape
+from layers import Conv2d, Linear, Maxpooling, Relu, LeakyRelu, Softmax, Reshape, BatchNorm
 from data import DataProvider
 from net import Net
 import numpy as np
@@ -10,18 +10,17 @@ from progressive.bar import Bar
 if __name__ == '__main__':
     (x, y), (xt, yt) = cifar10.load_data()
 
-    x = x.reshape(50000, 32, 32, 3).transpose(0,3,1,2)
-    xt = xt.reshape(10000, 32, 32, 3).transpose(0,3,1,2)
     def preprocessing(x):
         x = np.expand_dims(x, 1)
         x = x.astype(np.float)
-        x /= 255.0
+        x /= (x.max()/4)
         x -= x.mean()
-        x /= x.std()
         return x
 
     x = preprocessing(x)
     xt = preprocessing(xt)
+    print x.mean()
+    print x.std()
 
     x = x.reshape(50000, 32, 32, 3).transpose(0,3,1,2)
     xt = xt.reshape(10000, 32, 32, 3).transpose(0,3,1,2)
@@ -30,22 +29,19 @@ if __name__ == '__main__':
 
     # Model
     net = Net()
-    net.push(Conv2d(3, 3, 3, 8)) # 3x32x32 -> 8x30x30
-    net.push(LeakyRelu())
-    net.push(Conv2d(3, 3, 8, 8)) # 8x30x30 -> 8x28x28
-    net.push(LeakyRelu())
-    net.push(Maxpooling(2, 2)) # 8x28x28 -> 8x14x14
-    net.push(Conv2d(3, 3, 8, 16)) # 8x14x14 -> 16x12x12
-    net.push(LeakyRelu())
-    net.push(Conv2d(3, 3, 16, 16)) # 16x12x12 -> 16x10x10
-    net.push(LeakyRelu())
-    net.push(Conv2d(3, 3, 16, 16)) # 16x10x10 -> 16x8x8
-    net.push(LeakyRelu())
-    net.push(Maxpooling(2, 2)) # 16x8x8 -> 16x4x4
-    net.push(Reshape((256)))
-    net.push(Linear(256, 84))
-    net.push(LeakyRelu())
-    net.push(Softmax(84, 10))
+    net.push(Conv2d(5, 5, 3, 6)) # 3x32 -> 6x28
+    net.push(Relu())
+    net.push(BatchNorm())
+    net.push(Maxpooling(2, 2)) # 6x28 -> 6x14
+    net.push(Conv2d(5, 5, 6, 16)) # 6x14x14 -> 16x10x10
+    net.push(Relu())
+    net.push(BatchNorm())
+    net.push(Maxpooling(2, 2)) # 16x10x10 -> 16x5x5
+    net.push(Reshape((400)))
+    net.push(Linear(400, 200))
+    net.push(Relu())
+    net.push(BatchNorm())
+    net.push(Softmax(200, 10))
 
     # Data
     data = DataProvider()
@@ -54,12 +50,15 @@ if __name__ == '__main__':
     data.train_input(x[:n], y[:n])
     data.test_input(xt[:nt], yt[:nt])
     data.batch_size(32)
+    data.batch_size_test(1000)
 
     lr = 0.001
-    gamma = 0.99
-    mom = 0.99
-    l2_decay = 1e-4
-    total_epoch = 50
+    gamma = 1
+    mom = 0.95
+    l2_decay = 1e-3
+    total_epoch = 1000
+
+    loss_cache = 100
     for epoch in xrange(total_epoch):
         print 'Epoch: {}/{}'.format(epoch, total_epoch)
 
@@ -76,18 +75,29 @@ if __name__ == '__main__':
             bar.draw(value=data.get_count())
         t = time.time() - now
         acc, loss = net.get_record()
+        loss_avg = np.array(loss).mean()
+        loss_diff = loss_avg - loss_cache
+        loss_cache = loss_avg
         print 'Acc:    ', np.array(acc).mean()
-        print 'Loss:   ', np.array(loss).mean()
+        print 'Loss:   ', loss_avg
         print 'Time:   ', t
         f, b = net.get_profile()
         net.clear_record()
 
-        # Testing
-        net.input(data.test())
-        net.forward()
-        print 'Val:    ', net.get_record()[0][0]
-        print 'Loss:   ', net.get_record()[1][0]
+        bar_t = Bar(max_value=nt)
+        bar_t.cursor.clear_lines(2)  # Make some room
+        bar_t.cursor.save()  # Mark starting line
+        for _ in xrange(data.batch_run_test()):
+            net.input(data.next_batch_test())
+            net.forward()
+            bar_t.cursor.restore()  # Return cursor to start
+            bar_t.draw(value=data.get_count_test())
+        acc, loss = net.get_record()
+        print 'Val:    ', np.array(acc).mean()
+        print 'Loss:   ', np.array(loss).mean()
         net.clear_record()
+        lr *= gamma
+        print lr, loss_diff
         print
 
         # Profile
@@ -96,6 +106,3 @@ if __name__ == '__main__':
         #print 'Backward Time: ', sum(b)
         #print b
         #print
-
-        # Learning rate decay
-        lr *= gamma
