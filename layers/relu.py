7ff5c99f7e105bed7f7b1b1c1a6568bb40e0a2b9
Ming <rf37535@gmail.com>
2017-06-30 21:56:49 +0800

    Fix relu at zero point

--- layers/relu.py
+++ layers/relu.py
@@ -3,13 +3,13 @@ import numpy as np
 class Relu:
     def __init__(self):
         pass
-    
+
     def _forward(self, x):
-        self.mask = (x > 0).astype(int)
+        self.mask = (x >= 0).astype(int)
         return x * self.mask + 0.
-    
+
     def _backward(self, err, res):
         return err, self.mask
-    
+
     def _update(self, *args):
         pass
