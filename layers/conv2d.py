bcca7a49d3e82ddc932819a36359ebeb2ac5d7c7
Ming <rf37535@gmail.com>
2017-06-30 21:57:17 +0800

    Refine conv2d using 2D weights instead of 4D

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -1,21 +1,20 @@
 import numpy as np
 from gemm import im2col
+from math import sqrt
 
 class Conv2d:
     def __init__(self, kw, kh, input_channel, output_channel, pw=0, ph=0, sw=1, sh=1):
-        # Not implement so far
-        self.padding_width = pw
-        self.padding_height = ph
-        self.stride_width = sw
-        self.stride_height = sh
         # Control flag
         self.is_first_layer = False
         self.no_bias = False
         # Momentum
         self.pd_weight = 0
-        # Random kernel initialization
-        self.weights = np.random.normal(0, 1e-1, (input_channel, output_channel, kw, kh))
-        self.bias = np.random.normal(0, 1e-4, (1, output_channel, 1, 1))
+        self.hd_weight = 0
+        self.weights = np.random.rand(output_channel, input_channel * kw * kh) * sqrt(2.0/(input_channel * output_channel * kw * kh))
+        self.bias = np.ones((1, output_channel, 1, 1)) * 0.01
+        self.k = kw
+        self.ic = input_channel
+        self.oc = output_channel
 
     def set_first(self):
         self.is_first_layer = True
@@ -26,13 +25,13 @@ class Conv2d:
         return self.no_bias
 
     def _forward(self, x):
+        self.input = x
         self.input_shape = x.shape # Cache the input for backward use
-        out_map_size = np.array(x.shape[2:]) - np.array(self.weights.shape[2:]) + 1
-        out_map_size = list(self.weights.shape[1:2]) + list(x.shape[:1]) + list(out_map_size)
-        x_ = im2col(x, self.weights.shape[2], self.weights.shape[3], 0, 1)
+        out_map_size = np.array(x.shape[2:]) - np.array([self.k, self.k]) + 1
+        out_map_size = [self.weights.shape[0]] + [x.shape[0]] + list(out_map_size)
+        x_ = im2col(x, self.k, self.k, 0, 1)
         self.input_col = x_ # Cache the input for backward use
-        w_ = self.weights.transpose(1,2,3,0).reshape(self.weights.shape[1], -1)
-        output = w_.dot(x_).reshape(out_map_size).transpose(1,0,2,3)
+        output = self.weights.dot(x_).reshape(out_map_size).transpose(1,0,2,3)
         if self.no_bias:
             return output
         else:
@@ -40,24 +39,30 @@ class Conv2d:
 
     def _backward(self, err, res):
         delta = np.multiply(err, res)
-        delta_vector = delta.transpose(1,0,2,3).reshape(err.shape[1], -1)
-        s = self.weights.transpose(1,2,3,0).shape
-        self.d_weights = (delta_vector.dot(self.input_col.T) / err.shape[0]).reshape(s).transpose(3,0,1,2)
+        delta_vector = delta.reshape(delta.shape[1], -1)
+        self.d_weights = delta_vector.dot(self.input_col.T)
 
         if not self.no_bias:
-            self.d_bias = (np.sum(delta, axis=(0, 2, 3)) / err.shape[0])[None, :, None, None]
+            self.d_bias = np.sum(delta, axis=(0, 2, 3))[None, :, None, None]
 
         if not self.is_first_layer:
-            padding = self.weights.shape[3] - 1
-            delta_col = im2col(delta, self.weights.shape[2], self.weights.shape[3], padding, 1)
-            w_ = self.weights.transpose(0,2,3,1).reshape(self.weights.shape[0], -1)
-            return w_.dot(delta_col).reshape(self.input_shape), None
+            padding = self.k - 1
+            delta_col = im2col(delta, self.k, self.k, padding, 1)
+            w_ = self.weights.reshape(self.oc, self.ic, self.k, self.k)
+            w_ = w_.swapaxes(0, 1)
+            w_ = w_.reshape(w_.shape[0], -1)
+            output = w_.dot(delta_col)
+            output = output.reshape(self.oc, self.ic, -1).swapaxes(0, 1)
+            return output.reshape(self.input_shape), None
         else:
             return None, None
 
     def _update(self, step, mom, decay):
-        var = (self.pd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
-        self.pd_weight = var
-        self.weights += var
+        self.pd_weight = self.hd_weight
+        self.hd_weight = (self.hd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
+        self.weights += -mom * self.pd_weight + (1 + mom) * self.hd_weight
+        # var = (self.pd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
+        # self.pd_weight = var
+        # self.weights += var
         if not self.no_bias:
             self.bias -= step * self.d_bias
