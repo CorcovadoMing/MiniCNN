4671b74d2266067039e92a8b933a577ef0169dcf
Ming <rf37535@gmail.com>
2017-07-02 16:24:45 +0800

    Improve pooling layer

--- layers/maxpooling.py
+++ layers/maxpooling.py
@@ -11,17 +11,17 @@ class Maxpooling:
         B, N, h, w = x.shape
         output = np.empty((B, N, h/self.kernel_width, w/self.kernel_height))
         switch = np.empty(output.shape + (2,), dtype=np.int)
-        maxpooling(x, output, switch, self.kernel_width, self.kernel_height, 2, 2)
+        maxpooling(x, output, switch, self.kernel_width, self.kernel_height, self.kernel_width, self.kernel_height)
         self.masks = switch
         self.input_shape = x.shape
         return output
-    
+
     def _backward(self, err, res):
         output = np.empty(self.input_shape)
         unpooling(err, self.masks, output)
         return output, None
-    
+
     def _update(self, *args):
         pass
-            
+
 
