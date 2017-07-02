024fddd86ff24a683ca628ef9b4791b0efdd7889
Ming <rf37535@gmail.com>
2017-07-02 16:26:27 +0800

    Switch to adam optimizer

--- layers/linear.py
+++ layers/linear.py
@@ -6,6 +6,10 @@ class Linear:
     def __init__(self, i, c):
         self.pd_weight = 0
         self.hd_weight = 0
+
+        self.m = 0
+        self.v = 0
+        self.c = 1
         # Random kernel initialization
 #         self.weights = np.random.normal(0, 1e-1, (i, c))
 #         self.bias = np.random.normal(0, 1e-4, (c))
@@ -23,11 +27,17 @@ class Linear:
         self.d_bias = delta.sum(axis=0)
         return np.dot(delta, self.weights.T), None
 
-    def _update(self, step, mom, decay):
-        self.pd_weight = self.hd_weight
-        self.hd_weight = (self.hd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
-        self.weights += -mom * self.pd_weight + (1 + mom) * self.hd_weight
+    def _update(self, step, beta_1, beta_2, epoch):
+        # self.pd_weight = self.hd_weight
+        # self.hd_weight = (self.hd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
+        # self.weights += -mom * self.pd_weight + (1 + mom) * self.hd_weight
         # var = (self.pd_weight * mom) - (step * self.d_weights) - (step * decay * self.weights)
         # self.pd_weight = var
         # self.weights += var
+        self.m = beta_1 * self.m + (1 - beta_1) * self.d_weights
+        self.v = beta_2 * self.v + (1 - beta_2) * (self.d_weights ** 2)
+        m_hat = self.m / (1 - (beta_1 ** epoch))
+        v_hat = self.v / (1 - (beta_2 ** epoch))
+        self.weights = self.weights - ((step * m_hat) / (np.sqrt(v_hat) + 1e-8))
+
         self.bias -= step * self.d_bias
